da6cc4ec637c287be4fb84d7875d2b7b3a82cc5e
Dietmar Hamm <diemtarh@gmail.com>
2026-03-30 06:16:33 +0200

    Add images key to manifest for App Store cover image

--- dtx_analytic_wizard/__manifest__.py
+++ dtx_analytic_wizard/__manifest__.py
@@ -15,6 +15,9 @@
         'views/analytic_distribution_wizard_views.xml',
         'data/server_actions.xml',
     ],
+    'images': [
+        'static/description/banner.png',
+    ],
     'license': 'OPL-1',
     'price': 51.00,
     'currency': 'EUR',
